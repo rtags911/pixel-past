197fa2c133e0e8a17cf212abd1bf3457eedb42eb
rtags911 <rtagiobon@hyperstacksinc.com>
2025-05-27 00:16:52 +0800

    enhancement: Update & Add Native wind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,tsx,ts,jsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
